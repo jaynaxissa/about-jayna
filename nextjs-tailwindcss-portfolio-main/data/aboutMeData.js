984630e6c896285ce0f986c0e76e929b62e7ddd3
jaynaxissa <jaynaxissa@gmail.com>
2023-03-18 14:40:59 +0700

    first commit

---
+++ nextjs-tailwindcss-portfolio-main/data/aboutMeData.js
@@ -0,0 +1,16 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const aboutMeData = [
+  {
+    id: uuidv4(),
+    bio: "I am a purpose-driven individual who relentlessly pursues my passions based on my unwavering beliefs and deep-seated values. With over five years of exceptional professional experience in management and sales within the highly competitive UK beauty industry, I have honed my skills in identifying and capitalizing on opportunities that drive growth and success.",
+  },
+  {
+    id: uuidv4(),
+    bio: "As I previously worked as a project coordinator, I spearheaded a groundbreaking initiative to support small businesses in Thailand. Additionally, I launched my own mental health project to provide crucial support and resources to individuals in need. My unique ability to quickly identify and analyze complex problems, coupled with my unwavering commitment to finding innovative solutions, has led me to recognize the transformative potential of technology in addressing these issues.",
+  },
+  {
+    id: uuidv4(),
+    bio: "Through my journey, I have discovered my deep-rooted passion for helping women, children, and mental health issues. These causes are particularly close to my heart, and I am committed to leveraging technology to make a meaningful impact in these areas. I believe that by combining my passion with the transformative power of technology, I can make a significant difference in the lives of countless individuals who are facing immense challenges",
+  },
+];
